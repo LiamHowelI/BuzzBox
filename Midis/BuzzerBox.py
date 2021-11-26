d031321122969d89d0652b6281d2b9838b85d719
Liam Howell <c3304354@uon.edu.au>
2021-11-26 22:00:37 +1100

    Added BuzzBox module to Midi's folder

---
+++ Midis/BuzzerBox.py
@@ -0,0 +1,41 @@
+from PiicoDev_Buzzer import PiicoDev_Buzzer
+import utime
+
+def init_Buzzers(mul_addr = [0x5c]):
+    buzz = []
+    for i in mul_addr:
+        buzz.append(PiicoDev_Buzzer(volume=2, addr = i))
+    return buzz
+        
+def loadMusic(filename):
+    f = open(filename,'r')
+    music = []
+    for x in f:
+        x = x.replace('\r\n','')
+        new_list = x.split(',')
+        new_list = list(map(int, new_list))
+        try:
+            music.append(new_list)
+        except MemoryError:
+            print('MemErr')
+            break
+    f.close()
+    return music
+
+def playNote(noteLst, buzz):
+    buzz[noteLst[0]].tone(noteLst[1],noteLst[3])
+    
+def playMusic(music, buzz):
+    start = utime.ticks_ms()
+    i = 0
+    looping = True
+    while True: 
+        for i,note in enumerate(music):
+            curr_time = utime.ticks_ms()
+            if utime.ticks_diff(curr_time, start) >= note[2]:
+                playNote(note, buzz)
+                music.pop(i)
+            if len(music) <=0:
+                looping = False
+        if looping != True:
+            break
